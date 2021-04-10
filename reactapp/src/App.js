e34f10988eea54c7cec635f05c61baf10cb09ec7
Damiano Canali <canali.damiano@yahoo.com>
2021-04-10 11:09:56 -0400

    updates

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,46 +1,32 @@
 import scan from '../src/scan.png';
 import vaccine from '../src/vaccine.png';
 import news from '../src/news.png';
-import login from '../src/admin.png';
+import logout from '../src/admin.png';
 
 import './App.css';
 
-
 function App() {
   return (
-    <html>
-      <div className="App">
-        <div class="topnav">
-              <a href="app.js"><img alt="scan" src={scan}/> Home</a>
-              <a href="#index.html" target="_blank"><img alt="adminLogin" src={login}/> Admin Login</a>
-              <a href="#index.html" target="_blank"><img alt="vaccine"src={vaccine}/> Vaccine Finder</a>
-              <a href="#index.html" target="_blank"><img alt="news" src={news}/> News</a> 
-            </div>
-        <header className="App-header">
-          <head>
-            <meta charset="UTF-8"/>
-            <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
-            <meta content="width=device-width,initial-scale=1" name="viewport"/>
-            <link rel="shortcut icon" type="image/png" href="tempslogo.png"/>
-            <meta name="google" content="notranslate"/>
-          </head>
-          <body>	
-            <div class="buttons">
+          <div className="App">
+            <header className="App-header">
+               <div className="topnav">
+                  <a href="app.js"><img alt="scan" src={scan}/> Home</a>
+                  <a href="#index.html" target="_blank"><img alt="vaccine"src={vaccine}/> Vaccine Finder</a>
+                  <a href="#index.html" target="_blank"><img alt="news" src={news}/> News</a>
+                  <a href="logout.html" target="_blank"><img alt="logout" src={logout}/> Logout</a>
+                </div>
+              <div className="buttons">
                 <a href="index.html" class="myButton" target="_blank">EMPLOYEE</a>
-                <br />
+                <br/>
                 <a href="index.html" class="myButton2" target="_blank">GUEST</a>
-            </div>
-	<div>
-	<br/>
-	<br/>
-	</div>
-</body>
+              </div>
+              
+<footer className="style1">&copy; All reserved by Temps Company</footer>
 </header>
 </div>
-<footer class="style1">&copy; All reserved by Temps Company</footer>
-</html>
 
   );
+  
 }
 
 export default App;
