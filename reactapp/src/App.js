f6fd5a62446c791512c0abe19d7a80220e7920c7
Damiano Canali <canali.damiano@yahoo.com>
2021-05-05 17:19:01 -0400

    new code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,43 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Responsive.css';
-import { NavBar } from './NavBar';
-import { Footer } from './Footer';
 import './NavBar.css';
-import ReactDOM from 'react-dom';
+import NavBar from './NavBar';
+import { Footer } from './Footer';
+import { EmployeeButton } from './EmployeeButton';
+import { GuestButton } from './GuestButton';
 import EmployeePage from './Pages/Employee/EmployeePage';
+import GuestPage from './Pages/Guest/GuestPage';
 
 import{
   BrowserRouter as Router,
   Route,
-  Switch,
-  Redirect
+  Switch
 } from "react-router-dom";
-import { EmployeeButton } from './EmployeeButton';
-import { GuestButton } from './GuestButton';
 
-//Import Pages
-//import MainPage from "/Pages";
-//import EmployeePage from "./Pages/Employee/employee";
- 
 export class ProfilePage extends React.Component {
   render() {
     return (
       <div>
       <Router>
       <NavBar />
-       <Switch>
-         <Route exact path="/"><EmployeeButton />
-       <GuestButton /></Route>
+       <div className="mainContent">
+        <Switch>
+         <Route exact path="/">
+            <EmployeeButton />
+            <br /><br />
+            <br /><br />
+            <GuestButton />
+         </Route>
        <Route path="/employee"><EmployeePage /></Route>
+       <Route path="/guest"><GuestPage /></Route>
        </Switch>
+       </div>
        <Footer />
       </Router>
-      
       </div>
     );
    
   }
 }
-
-//ReactDOM.render(<ProfilePage />, document.getElementById('root'));
--- reactapp/src/GuestButton.js
+++ reactapp/src/GuestButton.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './NavBar.css';
 import { Card, CardBody, Button, CardText, CardTitle } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import{ Link } from "react-router-dom";
 
 export class GuestButton extends React.Component {
     render() {
         return(
-            <Card className="card" style={{ width: '18rem' }}>
+            <Card className="card" style={{ width: '20rem' }}>
             <CardBody className="cardBody">
                 <CardTitle className="cardTitle">Guest</CardTitle>
                 <CardText className="classText">
                     Check temperature of new guest and record it.
                 </CardText>
-                <Button variant="primary">Record</Button>
+                <Button><Link id="buttons" to='/guest'>Record</Link></Button>
             </CardBody>
             </Card>
             );
